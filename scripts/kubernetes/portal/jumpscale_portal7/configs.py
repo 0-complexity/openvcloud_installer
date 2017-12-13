62ebb9979a68e9f9ef3e0eb73c79cbf8de4e5133
root <tareka@codescalers.com>
2017-12-13 19:07:21 +0200

    WIP adding portal kubernetes componenets

---
+++ scripts/kubernetes/portal/jumpscale_portal7/configs.py
@@ -0,0 +1,17 @@
+from jumpscale import j
+from yaml import loader, load
+
+# this is not everything these are only the base components and can be added to
+
+
+
+def get_config():
+    with open('/opt/cfg/portal/portal-config-map.yaml') as f :
+        data  = load(f)
+    return data
+
+
+if __name__ == '__main__':
+    config = get_config()
+    cmd  = 'jsuser add -d admin:%s:admin:fakeemail.test.com:jumpscale' % config['portal']['passwd']
+    j.system.process(cmd)
