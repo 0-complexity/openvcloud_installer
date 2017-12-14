25a636c65cc2c4b048b599609dbc91412722da42
Maxime Daniel <root@maxux.net>
2017-12-14 17:20:23 +0100

    Adding datasource and dashboard for grafana
    Grafana now mount via configmap the datasources and the dashboards json

--- scripts/kubernetes/grafana/README.md
+++ scripts/kubernetes/grafana/README.md
@@ -1,5 +1,12 @@
 # Grafana kubernetes for openvcloud
 
+# Install initial datasource configuration
+- Add configmap datasource: `kubectl create configmap grafana-datasources --from-file=datasources/influxdb.yaml`
+
+# Install dashboards
+- Generate dashboard (if you changed them, generated are alreadu pushed): `cd dashboards && python3 build.py && cd ..`
+- Add dashboards configmap: `kubectl create configmap grafana-dashboards --from-file=dashboards`
+
 # Installation
-- Run `kubectl create -f service.yaml`
-- Run `kubectl create -f deployment.yaml`
+- Run `kubectl create -f grafana-service.yaml`
+- Run `kubectl create -f grafana-deployment.yaml`
---
+++ scripts/kubernetes/grafana/dashboards/build.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+template = "./templates"
+target = "./generated"
+datasource = "influxdb_main"
+
+for filename in os.listdir(template):
+    print("[+] generating dashboard: %s" % filename)
+
+    with open(os.path.join(template, filename), "r") as f:
+        contents = f.read()
+
+    db = json.loads(contents)
+
+    db['id'] = None
+    # db['title'] += " ({})".format(serviceObj.instance)
+
+    for row in db['rows']:
+        for panel in row['panels']:
+            panel['datasource'] = datasource
+
+    if 'templating' in db:
+        for item in db['templating']['list']:
+            item['datasource'] = datasource
+
+    with open(os.path.join(target, filename), "w") as f:
+        f.write(json.dumps(db))
