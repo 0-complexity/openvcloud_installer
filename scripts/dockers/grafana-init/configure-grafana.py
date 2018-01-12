f43eecea0a96fcb04c61af807bc2f0583d4d3b3c
root <tareka@codescalers.com>
2018-01-12 21:21:34 +0200

    added an init configuration for grafan deployment

---
+++ scripts/dockers/grafana-init/configure-grafana.py
@@ -0,0 +1,22 @@
+from JumpScale import j
+import yaml
+import netaddr
+import jinja2
+
+
+def get_config(path='/opt/cfg/system/system-config.yaml'):
+    with open(path) as file_discriptor:
+        return yaml.load(file_discriptor)
+
+
+def adjust_grafana_config(system_config={}):
+    if not system_config:
+        system_config = get_config()
+    template_config = "/opt/cfg/grafana/templates/grafana.ini"
+    config = '/etc/grafana/default.ini'
+    template = jinja2.Template(j.system.fs.fileGetContents(template_config))
+    j.system.fs.createEmptyFile(config)
+    j.system.fs.writeFile(config, template.render(**system_config))
+
+if __name__ == '__main__':
+    adjust_grafana_config()
