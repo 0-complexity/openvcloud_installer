533273781a7b9b00920f0a94f53b4bee63c41e71
Maxime Daniel <root@maxux.net>
2018-01-15 10:57:16 +0100

    Fix and improve pxeboot-base docker

--- scripts/dockers/pxeboot-base/prepare.sh
+++ scripts/dockers/pxeboot-base/prepare.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -ex
 
+autoclean="yes"
+
 wget https://github.com/PurePeople/911builder/archive/master.tar.gz
 tar -xvf master.tar.gz
 
 cd 911builder-master
 docker build -t 911builder:latest .
-docker run --privileged --rm -v $(pwd)/binaries:/binaries 911builder:latest
+docker run --privileged --rm -v $(pwd)/:/911builder -v $(pwd)/binaries:/binaries 911builder:latest
 cd ..
 
-cp -v 911builder-master/binaries/vmlinuz contents/tftpboot/911-vmlinuz
-cp -v 911builder-master/binaries/ramfs contents/tftpboot/911-ramfs
+rm -rf tftpboot/*
+
+cp -v 911builder-master/binaries/vmlinuz tftpboot/911-vmlinuz
+cp -v 911builder-master/binaries/ramfs tftpboot/911-ramfs
+
+if [ "${autoclean}" == "yes" ]; then
+    rm -f master.tar.gz
+    rm -rf 911builder-master
+fi
 
 echo ""
 echo "You can now use the Dockerfile to build the pxeboot-init image"
-echo "  docker build -t openvcloud/pxeboot-init ."
+echo "  docker build -t openvcloud/pxeboot-base ."
