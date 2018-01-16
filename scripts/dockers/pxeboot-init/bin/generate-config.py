4aaa846e2a749df74947a959830431bdb1c2e4cd
Maxime Daniel <root@maxux.net>
2018-01-16 10:37:38 +0100

    Add IPMI support on pxeboot

--- scripts/dockers/pxeboot-init/bin/generate-config.py
+++ scripts/dockers/pxeboot-init/bin/generate-config.py
@@ -1,18 +1,16 @@
 import yaml
 import os
 import subprocess
-from managementaddr import ManagementAddress
 
 configroot = '/opt/pxeboot/conf'
 tftproot = '/opt/pxeboot/tftpboot'
 imageroot = '/opt/pxeboot/images'
-intf = ManagementAddress()
 
 with open('/etc/global/system-config.yaml', 'r') as f:
     configfile = f.read()
 
 config = yaml.load(configfile)
-domain = "%s.%s" % (config['environment']['basedomain'], config['environment']['subdomain'])
+domain = "%s.%s" % (config['environment']['subdomain'], config['environment']['basedomain'])
 
 netmask = "24" # default to /24
 
@@ -23,7 +21,8 @@ else:
     netmask = config['network']['management']['network'].split('/')[1]
 
 gateway = "%s/%s" % (config['network']['management']['gateway'], netmask)
-subnet = config['network']['management']['network'].split('/')[0]
+mgmtsubnet = config['network']['management']['network'].split('/')[0]
+ipmisubnet = config['network']['ipmi']['network'].split('/')[0]
 
 """
 dhcphosts file
@@ -39,6 +38,11 @@ with open(target, "w") as f:
         for node in nodes:
             f.write("%s,%s,infinite\n" % (node['management']['macaddress'].lower(), node['name']))
 
+        f.write("\n# %s (ipmi)\n" % subgroup)
+
+        for node in nodes:
+            f.write("%s,%s,infinite\n" % (node['ipmi']['macaddress'].lower(), 'ipmi-%s' % node['name']))
+
         f.write("\n")
 
 """
@@ -52,8 +56,19 @@ with open(target, "w") as f:
 
         f.write("#\n# %s\n#\n" % subgroup)
 
+        nodenet = mgmtsubnet.split('.')
+
         for node in nodes:
-            f.write("%s %s.%s %s\n" % (node['management']['ipaddress'].split('/')[0], node['name'], domain, node['name']))
+            ipaddr = ipaddr = "%s.%s.%s.%s" % (nodenet[0], nodenet[1], nodenet[2], node['ip-lsb'])
+            f.write("%s %s.%s %s\n" % (ipaddr, node['name'], domain, node['name']))
+
+        f.write("\n# %s (ipmi)\n" % subgroup)
+
+        ipminet = ipmisubnet.split('.')
+
+        for node in nodes:
+            ipaddr = "%s.%s.%s.%s" % (ipminet[0], ipminet[1], ipminet[2], node['ip-lsb'])
+            f.write("%s %s.%s %s\n" % (ipaddr, 'ipmi-%s' % node['name'], domain, 'ipmi-%s' % node['name']))
 
         f.write("\n")
 
@@ -62,7 +77,8 @@ dnsmasq and pxelinux
 """
 netconfig = {
     '%domain%': domain,
-    '%range%': subnet,
+    '%range-lan%': mgmtsubnet,
+    '%range-ipmi%': ipmisubnet,
     '%gateway%': gateway,
     '%option6%': gateway,
 }
