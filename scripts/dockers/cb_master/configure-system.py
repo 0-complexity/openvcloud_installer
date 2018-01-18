93faf02bba7a1bcc018d6ace311b83a8c5c0e195
Abdulrahman <tareka@codescalers.com>
2018-01-18 04:21:13 +0200

    added flag to configure system ac hrd

--- scripts/dockers/cb_master/configure-system.py
+++ scripts/dockers/cb_master/configure-system.py
@@ -11,7 +11,7 @@ def get_config():
     return data
 
 
-def configure(roles, machineguid):
+def configure(roles, machineguid, controller_addr):
     """
     configures the js7 node.
     """
@@ -28,25 +28,26 @@ def configure(roles, machineguid):
     j.system.fs.copyDirTree('/opt/jumpscale7/hrd/system/', '/opt/cfg/system/')
     password = config['environment']['password']
     portal_client_services = j.atyourservice.findServices(domain='jumpscale', name='portal_client')
-    agentcontroller_service = j.atyourservice.get(domain='jumpscale', name='agentcontroller_client', instance='main')
-    osis_service = j.atyourservice.get(domain='jumpscale', name='osis', instance='main')
-    osis_client_service = j.atyourservice.get(domain='jumpscale', name='osis_client', instance='main')
-    portal_service = j.atyourservice.get(domain='jumpscale', name='portal', instance='main')
-    jsagent_service = j.atyourservice.get(domain='jumpscale', name='jsagent', instance='main')
+    agentcontroller_client_service = j.application.getAppInstanceHRD(name='agentcontroller_client', instance='main')
+    osis_service = j.application.getAppInstanceHRD(name='osis', instance='main')
+    osis_client_service = j.application.getAppInstanceHRD(name='osis_client', instance='main')
+    portal_service = j.application.getAppInstanceHRD(name='portal', instance='main')
 
     for portal_client_service in portal_client_services:
         portal_client_service.hrd.set('instance.param.secret', password)
         portal_client_service.hrd.save()
 
-    agentcontroller_service.hrd.set('instance.agentcontroller.client.passwd', password)
-    agentcontroller_service.hrd.save()
-    osis_service.hrd.set('instance.param.osis.superadmin.passwd', password)
-    osis_service.hrd.save()
-    osis_client_service.hrd.set('instance.param.osis.client.passwd', password)
-    osis_client_service.hrd.save()
-    portal_service.hrd.set('instance.param.portal.rootpasswd', password)
-    portal_service.hrd.set('instance.param.cfg.secret', password)
-    portal_service.hrd.save()
+    agentcontroller_client_service.set('instance.agentcontroller.client.passwd', password)
+    if controller_addr:
+        agentcontroller_client_service.set('instance.agentcontroller.client.addr', controller_addr)
+    agentcontroller_client_service.save()
+    osis_service.set('instance.param.osis.superadmin.passwd', password)
+    osis_service.save()
+    osis_client_service.set('instance.param.osis.client.passwd', password)
+    osis_client_service.save()
+    portal_service.set('instance.param.portal.rootpasswd', password)
+    portal_service.set('instance.param.cfg.secret', password)
+    portal_service.save()
 
     j.system.fs.copyDirTree('/opt/jumpscale7/hrd/apps/', '/opt/cfg/apps/')
 
@@ -56,5 +57,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--roles', default='node')
     parser.add_argument('--machineguid', default=None)
+    parser.add_argument('--controller-addr', dest="controller_addr", default=None)
     args = parser.parse_args()
-    configure(args.roles, args.machineguid)
+    configure(args.roles, args.machineguid, args.controller_addr)
