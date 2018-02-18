572b6a7f30a83e13fe06f775f329a92ea41e313e
alichaddad <chaddada@greenitglobe.com>
2018-02-18 12:40:19 +0200

    Upgrade cluster (#61)
    * WIP:added the upgrade pod and scripts related to nginx
    
    * WIP: new approach for nginx
    
    * wip upgrade cluster
    
    * adjusted the wait method for the kubctl apply also renamed the folders
    
    * fixed some bugs , and added the apply template cli endpoint
    
    * update pod hosting
    
    * Nginx upgrader (#60)
    
    * adjustments for nginx upgrader
    
    * bug fix
    
    * bug fix
    
    * removed revesion hardcoding
    
    * removed unnecessary certs

--- scripts/dockers/nginx-init/configure-nginx.py
+++ scripts/dockers/nginx-init/configure-nginx.py
@@ -2,6 +2,7 @@ from JumpScale import j
 import yaml
 import netaddr
 import jinja2
+import argparse
 
 
 def get_config(path='/opt/cfg/system/system-config.yaml'):
@@ -9,20 +10,24 @@ def get_config(path='/opt/cfg/system/system-config.yaml'):
         return yaml.load(file_discriptor)
 
 
-def adjust_nginx_config(system_config={}):
+def adjust_nginx_config(upgrade, system_config={}):
     if not system_config:
         system_config = get_config()
     config = "/opt/cfg/nginx/%s/%s"
     def ip_from_range(ip_range):
         net = netaddr.IPNetwork(ip_range)
         return net.ip
-
+    file_name = 'upgrade.conf' if upgrade else 'nginx.conf'
     loader = jinja2.FileSystemLoader(config % ("templates", ""))
     env = jinja2.Environment(autoescape=True, loader=loader)
     env.filters['ip_from_range'] = ip_from_range
-    template = env.get_template('nginx.conf')
+    template = env.get_template(file_name)
     j.system.fs.writeFile(config % ("sites-enabled", "nginx.conf"), template.render(**system_config))
 
 
 if __name__ == '__main__':
-    adjust_nginx_config()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--upgrade', help='apply the upgrade nginx config', default=False, action='store_true')
+    args = parser.parse_args()
+    adjust_nginx_config(args.upgrade)
+    j.system.fs.copyDirTree('/opt/code/github/openvcloud_installer/scripts/install/', '/opt/cfg/upgrade/')
