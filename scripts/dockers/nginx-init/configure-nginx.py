11be49afb33508d9d80ddb43f71dfee0a800ee9f
alichaddad <chaddada@greenitglobe.com>
2018-02-21 11:42:13 +0200

    persistent logs for updating

--- scripts/dockers/nginx-init/configure-nginx.py
+++ scripts/dockers/nginx-init/configure-nginx.py
@@ -3,6 +3,7 @@ import yaml
 import netaddr
 import jinja2
 import argparse
+import time
 
 
 def get_config(path='/opt/cfg/system/system-config.yaml'):
@@ -17,6 +18,14 @@ def adjust_nginx_config(upgrade, system_config={}):
     def ip_from_range(ip_range):
         net = netaddr.IPNetwork(ip_range)
         return net.ip
+    if upgrade:
+        log_dir = '/opt/logs/'
+        file_loc = log_dir + 'update_env.log'
+        if j.system.fs.exists(file_loc):
+            j.system.fs.unlink(file_loc)
+        tmp_name = log_dir + '{}.log'.format(int(time.time()))
+        j.system.fs.touch(tmp_name)
+        j.do.execute('ln -s {} {}'.format(tmp_name, file_loc))
     file_name = 'upgrade.conf' if upgrade else 'nginx.conf'
     loader = jinja2.FileSystemLoader(config % ("templates", ""))
     env = jinja2.Environment(autoescape=True, loader=loader)
