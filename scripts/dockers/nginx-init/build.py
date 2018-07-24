7492381f49f57aebe749bb8be708a64b24bba0aa
Jo De Boeck <deboeck.jo@gmail.com>
2018-07-24 22:30:34 +0200

    Add build scripts for images
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- scripts/dockers/nginx-init/build.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append('/opt/code/github/0-complexity/openvcloud_installer/scripts/')
-from buildlib.helper import build, clone_github_repo
-
-clone_github_repo('0-complexity', 'openvcloud_installer')
-build()
--- scripts/dockers/nginx/build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append('/opt/code/github/0-complexity/openvcloud_installer/scripts/')
-from buildlib.helper import clone_repo, build, clone_github_repo
-
-clone_github_repo('0-complexity', 'g8vdc')
-clone_github_repo('0-complexity', 'openvcloud_installer')
-clone_repo("https://docs.greenitglobe.com/binary/web_python", "master")
-build()
