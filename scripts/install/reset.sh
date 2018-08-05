8f2a8abc9b5930221abb5baaaeaa78bffa6073c1
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-08-05 11:32:56 +0200

    restart pods again if upgrade process fail  (#143)
    * start pods again when update fail
    
    * do not delete qa pod while updating

--- scripts/install/reset.sh
+++ scripts/install/reset.sh
@@ -1,5 +1,5 @@
-kubectl delete deploy --all --namespace default
+kubectl delete deploy --all --namespace default -l app!=qa
 kubectl delete configmap --all --namespace default
 kubectl delete secret --all --namespace default
-kubectl delete service --all --namespace default
+kubectl delete service --all --namespace default -l name!=qa
 kubectl delete statefulset --all --namespace default
