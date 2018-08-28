45fc9e6f7639bd494da393835968049f19161902
Jo De Boeck <deboeck.jo@gmail.com>
2018-08-28 18:55:38 +0200

    Update builder lib (#158)
    Make it possible to build all images
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- scripts/buildlib/builder.py
+++ scripts/buildlib/builder.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import yaml
 import requests
 import subprocess
@@ -7,26 +6,105 @@ import argparse
 from urllib.parse import urlparse
 
 MANIFESTURL = "https://raw.githubusercontent.com/0-complexity/home/master/manifests/"
+TAGAPI = "https://hub.docker.com/v2/repositories/openvcloud/{}/tags/"
 VERSION = "master"
+repodir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+
 
 MANIFESTDATA = {}
+
+
+def get_manifestdata():
+    if not MANIFESTDATA:
+        version = os.environ.get("VERSION", VERSION)
+        url = os.environ.get("MANIFESTURL", MANIFESTURL)
+        url += "{}.yml".format(version)
+        resp = requests.get(url)
+        resp.raise_for_status()
+        MANIFESTDATA.update(yaml.load(resp.content))
+    return MANIFESTDATA or {}
+
+
+class Image:
+    def __init__(self, name, version, path):
+        self.name = name
+        self.version = version
+        self.path = path
+
+    def get_parent(self):
+        dockerpath = os.path.join(self.path, "Dockerfile")
+        if not os.path.exists(dockerpath):
+            return
+        with open(dockerpath) as fd:
+            for line in fd.readlines():
+                if line.startswith("FROM"):
+                    image = line.split()[1]
+                    return image
+
+    def build(self):
+        print("Building {}".format(self.name))
+        builder = Builder(self.path)
+        builder.build()
+
+    def exists(self):
+        response = requests.get(TAGAPI.format(self.name))
+        response.raise_for_status()
+        for result in response.json()["results"]:
+            if result["name"] == self.version:
+                return True
+        return False
+
+    def __str__(self):
+        return "<Image: {}:{}>".format(self.name, self.version)
+
+    __repr__ = __str__
+
+
+class BuildAll:
+    def __init__(self):
+        self.images = []
+        self.load_images()
+
+    def load_images(self):
+        images = {}
+        for imagename, imageversion in get_manifestdata()["images"].items():
+            if imagename.startswith("openvcloud"):
+                name = imagename.split("/", 1)[-1].split(':')[0]
+                imagepath = os.path.join(repodir, "scripts", "dockers", name)
+                image = Image(name, imageversion, imagepath)
+                self.images.append(image)
+                images[name] = image
+
+        def sorter(img):
+            idx = 0
+            parent = img.get_parent()
+            while parent and parent.startswith("openvcloud"):
+                name = parent.split("/", 1)[-1].split(':')[0]
+                if name in images:
+                    idx += 1
+                    parent = images[name].get_parent()
+                else:
+                    break
+            return idx
+
+        self.images.sort(key=sorter)
+
+    def build(self):
+        for image in self.images:
+            if image.version == "latest" or not image.exists():
+                image.build()
 
 
 class Builder:
     def __init__(self, builddir):
         self.builddir = os.path.abspath(builddir)
         self.version = os.environ.get("VERSION", VERSION)
+        self._manifestdata = None
 
     def get_version(self, url):
-        if not MANIFESTDATA:
-            url = os.environ.get("MANIFESTURL", MANIFESTURL)
-            if self.version == "master":
-                return "branch", self.version
-            url += "{}.yml".format(self.version)
-            resp = requests.get(url)
-            resp.raise_for_status()
-            MANIFESTDATA.update(yaml.load(resp.content))
-        for repo in MANIFESTDATA["repos"]:
+        if self.version == "master":
+            return "branch", self.version
+        for repo in get_manifestdata():
             if url == repo["url"]:
                 return list(repo["target"].items())[0]
 
@@ -75,6 +153,10 @@ class Builder:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
+    parser.add_argument("--all", default=False, action="store_true")
     options = parser.parse_args()
-    builder = Builder(options.path)
+    if options.all:
+        builder = BuildAll()
+    else:
+        builder = Builder(options.path)
     builder.build()
